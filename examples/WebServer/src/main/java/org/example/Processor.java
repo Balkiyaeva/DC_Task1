12b3f10655c1793f82631e20084a7df2a952f250
balkiyaeva <amil.balkiyaeva@gmail.com>
2022-10-19 16:02:20 +0600

    recognizing different commands

--- examples/WebServer/src/main/java/org/example/Processor.java
+++ examples/WebServer/src/main/java/org/example/Processor.java
@@ -1,8 +1,12 @@
 package org.example;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Processor of HTTP request.
@@ -25,16 +29,75 @@ public class Processor {
         // To send response back to the client.
         PrintWriter output = new PrintWriter(socket.getOutputStream());
 
+        if (request.getRequestLine().contains("create")) {
+            process_create(output);
+        } else if (request.getRequestLine().contains("delete")) {
+            process_delete(output);
+        } else {
+            process_params(output);
+        }
+
+        socket.close();
+    }
+
+    public void process_create(PrintWriter output) throws IOException {
+
         // We are returning a simple web page now.
         output.println("HTTP/1.1 200 OK");
         output.println("Content-Type: text/html; charset=utf-8");
         output.println();
         output.println("<html>");
-        output.println("<head><title>Hello</title></head>");
-        output.println("<body><p>Hello, world!</p></body>");
+        output.println("<head><title>Create</title></head>");
+        output.println("<body><h1>Created<h1></body>");
         output.println("</html>");
         output.flush();
 
-        socket.close();
     }
+
+    public void process_delete(PrintWriter output) throws IOException {
+        AtomicInteger countLineWPlus = new AtomicInteger();
+        Path path = Paths.get("/Users/amira/Downloads");
+        Files.walk(path).forEach(path1 -> {
+            File file = path1.toFile();
+            if (file.isFile()) {
+                Scanner sc = null;
+                System.out.println(path1);
+                try {
+                    sc = new Scanner(file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                while (sc.hasNextLine())
+                    if (sc.nextLine().contains("+")){
+                        countLineWPlus.getAndIncrement();
+                    }
+            }
+        });
+
+        System.out.println(countLineWPlus);
+
+        output.println("HTTP/1.1 200 OK");
+        output.println("Content-Type: text/html; charset=utf-8");
+        output.println();
+        output.println("<html>");
+        output.println("<head><title>Delete</title></head>");
+        output.println("<body><h1>There are " + countLineWPlus + " lines contain '+' in the Downloads path<h1></body>");
+        output.println("</html>");
+        output.flush();
+    }
+
+    public void process_params(PrintWriter output) throws IOException {
+        output.println("HTTP/1.1 200 OK");
+        output.println("Content-Type: text/html; charset=utf-8");
+        output.println();
+        output.println("<html>");
+        output.println("<head><title>Params</title></head>");
+        output.println("""
+                <body><h1>«P.S. Пожалуста если с можите положыте на могилку цветы для Элджернона. На заднем дворе.»
+
+                Отрывок из книги: Дэниел Киз. «Цветы для Элджернона».<h1></body>""");
+        output.println("</html>");
+        output.flush();
+    }
+
 }
